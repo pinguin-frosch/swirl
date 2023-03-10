97f5bf442250058b5596971c36bd319d6f38ed89
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-03-10 13:14:54 -0300

    Ejecutar comandos en kde

--- main.go
+++ main.go
@@ -6,14 +6,16 @@ import (
 	"io"
 	"log"
 	"os"
+	"os/exec"
 	"path"
+	"strings"
 )
 
 // Default config location
 var configFile string = path.Join(os.Getenv("HOME"), ".config", "swirl", "config.json")
 
 func main() {
-	fmt.Println("===== Swirl =====")
+	fmt.Println("========================= Swirl =========================")
 
 	// Read config file
 	file, err := os.Open(configFile)
@@ -23,6 +25,7 @@ func main() {
 		return
 	}
 
+	// Close file when exiting
 	defer file.Close()
 
 	byteResult, err := io.ReadAll(file)
@@ -31,8 +34,46 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var res map[string]interface{}
-	json.Unmarshal([]byte(byteResult), &res)
+	var swirlConfig SwirlConfig
 
-	fmt.Println(res)
+	// Marshal json to a struct
+	json.Unmarshal(byteResult, &swirlConfig)
+
+	theme := swirlConfig.Theme
+	background := swirlConfig.Background
+
+	fmt.Printf("Changing theme to %s using %s background.\n", theme, background)
+
+	for _, app := range swirlConfig.Applications {
+		// Print app name
+		fmt.Printf("\n%s\n", strings.ToUpper(app.Name))
+
+		// Change background color
+		background_commands := app.Background[background]
+
+		for _, command := range background_commands {
+			fmt.Println(command)
+
+			// Form command with executable and the arguments
+			cmd := exec.Command(command[0], command...)
+			err := cmd.Run()
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+type SwirlConfig struct {
+	Theme        string        `json:"theme"`
+	Background   string        `json:"background"`
+	Applications []Application `json:"applications"`
+}
+
+type Application struct {
+	Name       string                 `json:"name"`
+	Variables  map[string]interface{} `json:"variables"`
+	Background map[string][][]string  `json:"background"`
+	Theme      map[string][][]string  `json:"theme"`
 }
