2121d681be6052a85d83089732fac703db3ecb7f
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-04-15 12:23:00 -0400

    Hacer los tipos más genéricos

--- types.go
+++ types.go
@@ -1,18 +1,12 @@
 package main
 
 type SwirlConfig struct {
-	Variables  SwirlVariables `json:"variables"`
-	Background []Application  `json:"background"`
-	Theme      []Application  `json:"theme"`
-	Keyboard   []Application  `json:"keyboard"`
-	Taskbar    []Application  `json:"taskbar"`
+	Variables SwirlVariables           `json:"variables"`
+	Commands  map[string][]Application `json:"commands"`
 }
 
 type SwirlVariables struct {
-	Theme        string                       `json:"theme"`
-	Background   string                       `json:"background"`
-	Keyboard     string                       `json:"keyboard"`
-	Taskbar      string                       `json:"taskbar"`
+	Global       map[string]string            `json:"global"`
 	Applications map[string]map[string]string `json:"applications"`
 }
 
